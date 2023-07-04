25e6a3cb1abf610e255bd75824bd65e84f1bb4c6
Snowy <95209011+jgzhang3397@users.noreply.github.com>
2023-07-04 18:58:58 +0200

    Add files via upload

--- script/Contact1.cs
+++ script/Contact1.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class Contact1 : MonoBehaviour
 {
     private Animator animator;
     private bool flag = false;
     private int counter = 0;
+    public  Text scoreText;
+    public Text depthText;
+    public Text progressText;
+    public static float distanceTravelled = 0;
+    Vector2 lastPosition;
+    public Rigidbody2D rb;
+
+
     void Start()
     {
+        Scoring.totalScore = 0;
+        Scoring.remain = 0.0f;
+        Scoring.progress = 0;
+        distanceTravelled = 0.0f;
         animator = GetComponent<Animator>();
+        lastPosition = transform.position;//add
+        scoreText.text = "Score: " + Scoring.totalScore;//add
+        depthText.text = "Remain: " + Scoring.remain;
+        progressText.text = "Progress: " + Scoring.progress + "%";
         
+        
+
     }
     //animator.SetBool("conllision", false);
  
@@ -19,25 +39,57 @@ public class Contact1 : MonoBehaviour
     {
         animator.SetBool("conllision", flag);
         Invoke("delayOpen", 5);
-        
-        // 5. Das dritte Hindernis (welches der Bohrer berührt) beendet das Spiel.
         if (counter>=3)
-        {
-            // 7. Bei Spielende gelangen Spieler:innen zurück ins Menü.
+            {
+            SceneManager.LoadScene("menu");
+            
+        }
+        if(Scoring.totalScore == 200) {
             SceneManager.LoadScene("menu");
         }
+        distanceTravelled += Mathf.Abs(transform.position.y - lastPosition.y);//add
+        lastPosition = transform.position;//add
+        Scoring.totalScore = (int)Math.Round(distanceTravelled);//add
+        Scoring.remain = 200 - Scoring.totalScore;
+        Scoring.progress = Scoring.totalScore / 2;
+        
+        scoreText.text = "Score: " + Scoring.totalScore;//add
+        
+        //if (distanceTravelled > BackgroundChange.mantle)
+        //{
+            depthText.text = "Remain: " + Scoring.remain;
+            progressText.text = "Progress: " + Scoring.progress + "%";
+       // }
+
+       
     }
+
+    IEnumerator DoRotate() {
+    for (int i = 0; i <= 16; ++i) {
+        rb.MoveRotation(45 * i);
+        yield return new WaitForSeconds(.5f);
+        
+     }
+}
     private void OnCollisionEnter2D(Collision2D other)
     {
 
         flag = true;
         counter += 1;
+        DoRotate();
+        Heart.HealthBar();
+       
+        
         
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         flag = true;
         counter += 1;
+        DoRotate();
+        Heart.HealthBar();
+        
+
     }
     void delayOpen()
     {
